6c3146d507cff4cddbe0cfef8804709c60f6933c
Younes Charfaoui <mxcsyounes@gmail.com>
2023-04-19 06:16:09 +0100

    Removing uneeded classes, renaming the package

--- app/src/main/java/com/tarkalabs/ui/MainActivity.kt
+++ app/src/main/java/com/tarkalabs/ui/MainActivity.kt
@@ -16,25 +16,7 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Eam360uiandroidTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
-      }
+
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  Eam360uiandroidTheme {
-    Greeting("Android")
   }
 }
