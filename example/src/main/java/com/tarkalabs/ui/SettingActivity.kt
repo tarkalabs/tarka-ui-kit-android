0630774f05fd2c6022dc2b73a89d7785cef881bd
Younes Charfaoui <mxcsyounes@gmail.com>
2023-04-24 08:23:13 +0100

    Completing the strucuture, need theming

--- example/src/main/java/com/tarkalabs/ui/SettingActivity.kt
+++ example/src/main/java/com/tarkalabs/ui/SettingActivity.kt
@@ -4,26 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Badge
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,7 +56,36 @@ class SettingActivity : ComponentActivity() {
     }) { paddingValues ->
       Column(modifier = Modifier.padding(paddingValues)) {
         SettingHeader()
+        Lookup(label = "Role", body = "Supervisor")
+        Divider()
+        SettingItem {
+          SettingItemContent(Icons.Default.Search, "Transaction errors", 3)
+        }
+        SettingItem {
+          SettingItemContent(Icons.Default.Search, "Sync status")
+        }
+        SettingItem {
+          SettingItemContent(Icons.Default.Search, "Tabs configuration")
+        }
+        SettingItem {
+          SettingItemContent(Icons.Default.Search, "Export log")
+        }
       }
+    }
+  }
+
+  @Composable
+  fun SettingItemContent(
+    leadingIcon: ImageVector,
+    title: String,
+    badgeCount: Int? = null,
+  ) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+      Icon(imageVector = leadingIcon, contentDescription = null)
+      Text(text = title, Modifier.weight(1f))
+      if (badgeCount != null)
+        Badge { Text(text = badgeCount.toString()) }
+      Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
     }
   }
 
@@ -60,10 +94,18 @@ class SettingActivity : ComponentActivity() {
   fun SettingHeader() {
     // TODO: handle the center of the avatar
     Row {
-      Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = drawable.avatar), contentDescription = null, modifier = Modifier
-          .clip(CircleShape)
-          .size(96.dp))
+      Column(
+        modifier = Modifier.weight(1f),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+        Image(
+          painter = painterResource(id = drawable.avatar),
+          contentDescription = null,
+          modifier = Modifier
+            .clip(CircleShape)
+            .size(96.dp)
+        )
         Text(text = "Ronald Richards")
         Text(text = "BEDFORD")
       }
@@ -73,18 +115,40 @@ class SettingActivity : ComponentActivity() {
     }
   }
 
+  @Preview(showBackground = true)
+  @Composable
+  fun Lookup(onClick: () -> Unit = {}, label: String = "", body: String = "") {
+    Row(modifier = Modifier.clickable {
+      onClick()
+    }, verticalAlignment = Alignment.CenterVertically) {
+      Column(Modifier.weight(1f)) {
+        Text(text = label)
+        Text(text = body)
+      }
+      Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
+    }
+  }
+
+  @Composable
+  fun SettingItem(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Surface(modifier.heightIn(min = 40.dp)) {
+      content()
+    }
+  }
+
   @Composable
   fun TopBarNavigationIcon(imageVector: ImageVector, onClick: () -> Unit = {}) {
     IconButton(onClick = onClick) {
       Icon(imageVector = imageVector, contentDescription = null)
     }
   }
+
   @Composable fun EAMTopBar(
     leadingIcon: @Composable () -> Unit,
     titleContent: @Composable () -> Unit,
   ) {
     TopAppBar(
-      navigationIcon =  leadingIcon,
+      navigationIcon = leadingIcon,
       title = titleContent,
     )
   }
