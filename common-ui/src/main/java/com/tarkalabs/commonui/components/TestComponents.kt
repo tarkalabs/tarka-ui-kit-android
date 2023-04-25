c37df4521fc8a598630cd69b95f275c3aff56e8c
AskNilesh <rathodnilsrk@gmail.com>
2023-04-25 12:19:40 +0530

    - Added color resources and theme using material 3 designs

--- common-ui/src/main/java/com/tarkalabs/commonui/components/TestComponents.kt
+++ common-ui/src/main/java/com/tarkalabs/commonui/components/TestComponents.kt
@@ -1,11 +1,65 @@
 package com.tarkalabs.commonui.components
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tarkalabs.commonui.theme.Eam360Theme
+import com.tarkalabs.commonui.theme.Eam360uiandroidTheme2
+
+@Preview(showBackground = true) @Composable fun TestText(text: String = "Hello There") {
+  Text(text, style = Eam360Theme.typography.button6)
+}
+
+@Composable fun TestButton() {
+  EAMButton(modifier = Modifier
+    .fillMaxWidth()
+    .padding(10.dp), onClick = {}) {
+    Text(text = "Hello")
+    // Text(text = "World", style = Eam360Theme.typography.button6)
+  }
+}
 
 @Composable
-fun TestText(text: String) {
-  Text(text, style = TextStyle(fontSize = 88.sp))
+@Preview(showBackground = true, name ="Custom Button")
+fun Testing() {
+  Eam360uiandroidTheme2 {
+    EAMButton(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp),
+      onClick = {},
+      colors = ButtonDefaults.buttonColors(
+        containerColor = Color.Green
+      )
+
+    ) {
+      Text(text = "Custom Button")
+      // Text(text = "World", style = Eam360Theme.typography.button6)
+    }
+
+  }
+}
+
+@Composable fun EAMButton(
+  onClick: () -> Unit = {},
+  modifier: Modifier = Modifier,
+  colors : ButtonColors = ButtonDefaults.buttonColors(),
+  content: @Composable () -> Unit,
+) {
+  Button(colors = colors, onClick = onClick, modifier = modifier, content = {
+    ProvideTextStyle(
+      value = Eam360Theme.typography.heading1
+    ) {
+      content()
+    }
+  })
 }
--- common-ui/src/main/java/com/tarkalabs/commonui/theme/Theme.kt
+++ common-ui/src/main/java/com/tarkalabs/commonui/theme/Theme.kt
@@ -1,36 +1,46 @@
 package com.tarkalabs.commonui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-  primary = Purple200,
-  primaryVariant = Purple700,
+private val DarkColorPalette = darkColorScheme(
+  primary = Color.Red,
+  primaryContainer = Color.Green,
   secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
-  primary = Purple500,
-  primaryVariant = Purple700,
-  secondary = Teal200
+private val LightColorPalette = lightColorScheme(
+  primary = ColorPrimary,
+  onPrimary = ColorOnPrimary,
+  primaryContainer = ColorAltPrimary,
+  onPrimaryContainer = ColorOnAltPrimary,
 
-  /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+  secondary = ColorSecondary,
+  onSecondary = ColorOnSecondary,
+  secondaryContainer = ColorAltSecondary,
+  onSecondaryContainer = ColorOnAltSecondary,
 
-@Composable
-fun Eam360uiandroidTheme(
-  darkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable () -> Unit
+  tertiary = ColorTertiary,
+  onTertiary = ColorOnTertiary,
+  onTertiaryContainer = ColorAltTertiary,
+  tertiaryContainer = ColorOnAltTertiary,
+
+  error = ColorError,
+  onError = ColorOnError,
+
+  background = Color.White,
+  onBackground = Color.Black,
+  surface = Color.White,
+  onSurface = Color.Black,
+  )
+
+@Composable fun Eam360uiandroidTheme(
+  darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit
 ) {
   val colors = if (darkTheme) {
     DarkColorPalette
@@ -38,10 +48,38 @@ fun Eam360uiandroidTheme(
     LightColorPalette
   }
 
-  MaterialTheme(
-    colors = colors,
-    typography = Typography,
-    shapes = Shapes,
-    content = content
-  )
+
+  CompositionLocalProvider(
+    LocalExtendedTypography provides extendedTypography
+  ) {
+    MaterialTheme(
+      typography = Typography, content = content, colorScheme = colors, shapes = Shapes
+    )
+  }
+}
+
+
+@Composable fun Eam360uiandroidTheme2(
+  darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit
+) {
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    DarkColorPalette
+  }
+
+
+  CompositionLocalProvider(
+    LocalExtendedTypography provides extendedTypography
+  ) {
+    MaterialTheme(
+      typography = Typography, content = content, colorScheme = colors, shapes = Shapes
+    )
+  }
+}
+
+object Eam360Theme {
+  val typography: ExtendedTypography
+    @Composable
+    get() = LocalExtendedTypography.current
 }
