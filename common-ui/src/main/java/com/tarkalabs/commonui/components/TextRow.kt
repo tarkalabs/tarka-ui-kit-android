6a1f7c584eab6da206c0b126503176ad19a8e39e
Younes Charfaoui <mxcsyounes@gmail.com>
2023-04-26 12:27:20 +0100

    Adding the text row

---
+++ common-ui/src/main/java/com/tarkalabs/commonui/components/TextRow.kt
@@ -0,0 +1,64 @@
+package com.tarkalabs.commonui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.R.drawable
+import com.tarkalabs.common_ui.R
+
+@Composable
+fun TextRowWithDescription(
+  title : String,
+  description : String,
+  modifier: Modifier = Modifier.fillMaxWidth(),
+  @DrawableRes iconOne : Int? = null,
+  @DrawableRes iconTwo : Int? = null,
+  buttonTitle : String? = null,
+  @DrawableRes infoIcon : Int? = null,
+
+) {
+  Row(modifier, verticalAlignment = Alignment.CenterVertically) {
+    Column(Modifier.weight(1f)) {
+      Text(text = title)
+      Text(text = description)
+    }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+      if (iconOne != null)
+        EamGhostIconButton(icon = iconOne)
+      if (iconTwo != null)
+        EamGhostIconButton(icon = iconTwo)
+      if (buttonTitle != null) {
+        OutlinedButton(onClick = { /*TODO*/ }) {
+          Text(text = buttonTitle)
+        }
+      }
+      if (infoIcon != null){
+        IconButton(onClick = { /*TODO*/ }) {
+          Icon(painter = painterResource(id = infoIcon), contentDescription = null)
+        }
+      }
+    }
+
+  }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TextRowWithDescriptionPreview() {
+  TextRowWithDescription(title = "Title", description = "Description", infoIcon = drawable.ic_call_answer,
+    iconOne = drawable.ic_call_answer,
+    iconTwo = drawable.ic_call_answer,
+    buttonTitle = "Label"
+  )
+}
