85f82ccd182c09bc74ca7f1a5465e4800807ec46
Charfaoui Younes <mxcsyounes@gmail.com>
2023-05-05 08:21:35 +0100

    Updating for Raja comments

--- common-ui/build.gradle.kts
+++ common-ui/build.gradle.kts
@@ -47,8 +47,6 @@ dependencies {
   implementation("androidx.core:core-ktx:1.10.0")
   implementation( "androidx.compose.ui:ui:$composeUiVersion")
   implementation( "androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
-  // implementation( "androidx.compose.material:material:$composeUiVersion")
-  // implementation("com.google.android.material:material:1.8.0")
   implementation ("androidx.compose.material3:material3:1.1.0-rc01")
 
   testImplementation( "junit:junit:4.13.2")
