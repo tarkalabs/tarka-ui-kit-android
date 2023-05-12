526b36e7f08ef351e3b46d64e5c4b96ea1f2844d
Raja Jawahar <rajajawahar77@gmail.com>
2023-05-12 17:17:22 +0530

    Usused import removed

--- common-ui/src/main/java/com/tarkalabs/uicomponents/components/Badge.kt
+++ common-ui/src/main/java/com/tarkalabs/uicomponents/components/Badge.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
