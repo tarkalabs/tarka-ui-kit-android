9617636faef6314ca8bff5524093014415c09c5b
Raja Jawahar <rajajawahar77@gmail.com>
2023-05-12 15:50:19 +0530

    Preview layout aded for Badge

--- common-ui/src/main/java/com/tarkalabs/uicomponents/components/Badge.kt
+++ common-ui/src/main/java/com/tarkalabs/uicomponents/components/Badge.kt
@@ -1,13 +1,16 @@
 package com.tarkalabs.uicomponents.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -56,5 +59,16 @@ enum class BadgeSize(val size: Dp) {
 }
 
 @Preview(showBackground = true) @Composable fun BadgePreview() {
-  Badge(count = 4440440, badgeSize = M)
+  Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.SpaceEvenly
+
+  ) {
+    Spacer(modifier = Modifier.padding(2.dp))
+    Badge(badgeSize = S)
+    Spacer(modifier = Modifier.padding(2.dp))
+    Badge(count = 4, badgeSize = M)
+    Spacer(modifier = Modifier.padding(2.dp))
+    Badge(count = 444, badgeSize = L)
+  }
 }
