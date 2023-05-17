673620887a729de36b720b39b78f1f66dcd71c52
Raja Jawahar <rajajawahar77@gmail.com>
2023-05-17 20:50:53 +0530

    Package name modified to tarkaui

--- tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/TopBarScreenShotTest.kt
+++ tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/TopBarScreenShotTest.kt
@@ -1,7 +1,5 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 
-package com.tarkalabs.commonui
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.karumi.shot.ScreenshotTest
@@ -16,11 +14,10 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class TopBarScreenShotTest : ScreenshotTest {
 
-  @get:Rule
-  val composeRule = createComposeRule()
+  @get:Rule val composeRule = createComposeRule()
 
-  @Test
-  fun renderTopAppBarTextOnly() {
+  @OptIn(ExperimentalMaterial3Api::class)
+  @Test fun renderTopAppBarTextOnly() {
     composeRule.setContent {
       TopBar("Screenshot")
     }
@@ -28,12 +25,10 @@ class TopBarScreenShotTest : ScreenshotTest {
     compareScreenshot(composeRule)
   }
 
-  @Test
-  fun renderTopAppBarTextWithIcon() {
+  @Test fun renderTopAppBarTextWithIcon() {
     composeRule.setContent {
       TopBar(
-        "Screenshot",
-        navigationIcon = TarkaIcon(R.drawable.keyboard_arrow_right, "Arrow Right")
+        "Screenshot", navigationIcon = TarkaIcon(R.drawable.keyboard_arrow_right, "Arrow Right")
       )
     }
 
