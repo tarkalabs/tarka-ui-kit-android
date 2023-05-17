622e35188bb9fb63c6e5919a022fc5a398818e2e
Raja Jawahar <rajajawahar77@gmail.com>
2023-05-17 12:24:46 +0530

    TarkaIcon used instead of Drawable Resource

--- common-ui/src/main/java/com/tarkalabs/uicomponents/components/TopBar.kt
+++ common-ui/src/main/java/com/tarkalabs/uicomponents/components/TopBar.kt
@@ -1,6 +1,5 @@
 package com.tarkalabs.uicomponents.components
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -18,15 +17,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import com.tarkalabs.uicomponents.models.TarkaIcon
 import com.tarkalabs.uicomponents.theme.Eam360Theme
 
 @OptIn(ExperimentalMaterial3Api::class) @Composable fun TopBar(
   title: String,
-  @DrawableRes navigationIcon: Int? = null,
-  @DrawableRes searchIcon: Int? = null,
-  @DrawableRes menuItemIconOne: Int? = null,
-  @DrawableRes menuItemIconTwo: Int? = null,
-  @DrawableRes menuItemIconThree: Int? = null,
+  navigationIcon: TarkaIcon? = null,
+  searchIcon: TarkaIcon? = null,
+  menuItemIconOne: TarkaIcon? = null,
+  menuItemIconTwo: TarkaIcon? = null,
+  menuItemIconThree: TarkaIcon? = null,
   onNavigationIconClick: () -> Unit = {},
   onFirstMenuItemClicked: () -> Unit = {},
   onSecondMenuItemClicked: () -> Unit = {},
@@ -53,7 +53,10 @@ import com.tarkalabs.uicomponents.theme.Eam360Theme
     navigationIcon = {
       if (navigationIcon != null) {
         IconButton(onClick = onNavigationIconClick) {
-          Icon(painter = painterResource(id = navigationIcon), contentDescription = "Back arrow")
+          Icon(
+            painter = painterResource(id = navigationIcon.iconRes),
+            contentDescription = "Back arrow"
+          )
         }
       }
     },
@@ -62,14 +65,14 @@ import com.tarkalabs.uicomponents.theme.Eam360Theme
         IconButton(onClick = {
           showSearchBar = true
         }) {
-          Icon(painter = painterResource(id = searchIcon), contentDescription = "Search")
+          Icon(painter = painterResource(id = searchIcon.iconRes), contentDescription = "Search")
         }
       }
 
       if (menuItemIconThree != null) {
         IconButton(onClick = onThirdMenuItemClicked) {
           Icon(
-            painter = painterResource(id = menuItemIconThree),
+            painter = painterResource(id = menuItemIconThree.iconRes),
             contentDescription = "Menu Item  three"
           )
         }
@@ -78,7 +81,8 @@ import com.tarkalabs.uicomponents.theme.Eam360Theme
       if (menuItemIconTwo != null) {
         IconButton(onClick = onSecondMenuItemClicked) {
           Icon(
-            painter = painterResource(id = menuItemIconTwo), contentDescription = "Menu Item two"
+            painter = painterResource(id = menuItemIconTwo.iconRes),
+            contentDescription = "Menu Item two"
           )
         }
       }
@@ -86,7 +90,8 @@ import com.tarkalabs.uicomponents.theme.Eam360Theme
       if (menuItemIconOne != null) {
         IconButton(onClick = onFirstMenuItemClicked) {
           Icon(
-            painter = painterResource(id = menuItemIconOne), contentDescription = "Menu Item one"
+            painter = painterResource(id = menuItemIconOne.iconRes),
+            contentDescription = "Menu Item one"
           )
         }
       }
@@ -100,7 +105,7 @@ import com.tarkalabs.uicomponents.theme.Eam360Theme
 }
 
 @OptIn(ExperimentalMaterial3Api::class) @Composable fun EamNormalTopBar(
-  title: String, @DrawableRes navigationIcon: Int? = null
+  title: String, navigationIcon: TarkaIcon? = null
 ) {
   TopBar(title = title, navigationIcon = navigationIcon)
 }
