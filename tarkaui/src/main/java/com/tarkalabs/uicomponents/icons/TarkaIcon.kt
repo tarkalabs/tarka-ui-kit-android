65e0310e46fe2ab63bb33b156baad8735f95db6f
Younes Charfaoui <mxcsyounes@gmail.com>
2023-05-25 20:04:11 +0100

    Making the constructor private to the module, restricing the copy of the icon res

--- tarkaui/src/main/java/com/tarkalabs/uicomponents/icons/TarkaIcon.kt
+++ tarkaui/src/main/java/com/tarkalabs/uicomponents/icons/TarkaIcon.kt
@@ -3,10 +3,36 @@ package com.tarkalabs.uicomponents.icons
 import androidx.annotation.DrawableRes
 import com.microsoft.fluent.mobile.icons.R
 
-data class TarkaIcon constructor(
+class TarkaIcon internal constructor(
   @DrawableRes val iconRes: Int,
   val contentDescription: String,
-)
+){
+  fun copy(contentDescription: String): TarkaIcon {
+    return TarkaIcon(this.iconRes, contentDescription)
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as TarkaIcon
+
+    if (iconRes != other.iconRes) return false
+    if (contentDescription != other.contentDescription) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = iconRes
+    result = 31 * result + contentDescription.hashCode()
+    return result
+  }
+
+  override fun toString(): String {
+    return "TarkaIcon(iconRes=$iconRes, contentDescription='$contentDescription')"
+  }
+}
 
 object TarkaIcons {
 
