550f552303489f7ea7adec99b3f28718a788e994
AskNilesh <rathodnilsrk@gmail.com>
2023-06-06 14:57:19 +0530

    - Added documentation and test tags for TUIAvatar

--- tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUIAvatar.kt
+++ tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUIAvatar.kt
@@ -12,10 +12,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tarkalabs.uicomponents.Tags
 import com.tarkalabs.uicomponents.components.AvatarSize.L
 import com.tarkalabs.uicomponents.components.AvatarSize.M
 import com.tarkalabs.uicomponents.components.AvatarSize.S
@@ -43,12 +45,23 @@ sealed class AvatarType{
   data class Image(val image : ImageBitmap) : AvatarType()
 }
 
+/**
+* A composable function that displays an avatar image.
+ * @param modifier The modifier to be applied to the avatar.
+ * @param avatarType The type of avatar to be displayed. It can be an Icon, Image, or Text.
+ * @param avatarSize The size of the avatar. It can be AvatarSize.S, AvatarSize.M, or AvatarSize.L. The default value is AvatarSize.L.
+ * @param showBadge Determines whether to show a badge on the avatar. The default value is false.
+ * @param avatarTag The test tag for the Avatar.
+ * @param badgeTag  The test tag for the Avatar Badge.
+ */
 @Composable
 fun TUIAvatar(
   modifier: Modifier = Modifier,
   avatarType: AvatarType,
   avatarSize: AvatarSize = L,
-  showBadge: Boolean = false
+  showBadge: Boolean = false,
+  avatarTag : String = Tags.TAG_AVATAR,
+  badgeTag : String = Tags.TAG_AVATAR_BADGE,
 ) {
   Box(
     modifier = modifier.size(avatarSize.size),
@@ -58,7 +71,8 @@ fun TUIAvatar(
       modifier = modifier
         .size(avatarSize.size)
         .clip(CircleShape)
-        .background(TUITheme.colors.tertiary),
+        .background(TUITheme.colors.tertiary)
+        .testTag(avatarTag),
       contentAlignment = Alignment.Center
     ) {
       when(avatarType){
@@ -83,7 +97,7 @@ fun TUIAvatar(
     if (showBadge) {
       val badgeSize = badgeSizeFor(avatarSize)
       TUIBadge(
-        modifier = Modifier.align(Alignment.BottomEnd),
+        modifier = Modifier.align(Alignment.BottomEnd).testTag(badgeTag),
         badgeSize = badgeSize,
         color = TUITheme.colors.success
       )
