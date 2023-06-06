e6eecd210ac933ef18ee0e447c68d2eb426ae5d1
AskNilesh <rathodnilsrk@gmail.com>
2023-06-06 14:48:58 +0530

    - Fixed review comments

--- tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/TUINavigationRowTest.kt
+++ tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/TUINavigationRowTest.kt
@@ -6,9 +6,6 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithTag
-import androidx.compose.ui.test.onFirst
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -22,33 +19,9 @@ import org.mockito.kotlin.verify
 class TUINavigationRowTest {
   @get:Rule val composable = createComposeRule()
 
-  private val BADGE_TAG = "BADGE_TAG"
   private val ROW_TAG = "ROW_TAG"
 
   @Test fun navigationRow_Elements_Displayed() {
-
-    composable.setContent {
-      TUINavigationRow(
-        title = "Label",
-        leadingIcon = TarkaIcons.CheckMark,
-        badgeCount = 5,
-        showRightArrow = true,
-        onClick = {},
-        badgeTestTag = BADGE_TAG,
-        rowTestTag = "rowTestTag"
-      )
-
-    }
-
-    composable.onNodeWithText("Label").assertIsDisplayed()
-    composable.onNodeWithTag("rowTestTag").assertIsDisplayed()
-    composable.onAllNodesWithTag(BADGE_TAG, useUnmergedTree = true).onFirst().assertIsDisplayed()
-    composable.onNodeWithContentDescription(TarkaIcons.ChevronRight.contentDescription).assertIsDisplayed()
-    composable.onNodeWithContentDescription(TarkaIcons.CheckMark.contentDescription, useUnmergedTree = true).assertIsDisplayed()
-
-  }
-
-  @Test fun navigationRow2_Elements_Displayed() {
 
     composable.setContent {
       TUINavigationRow(
@@ -75,14 +48,13 @@ class TUINavigationRowTest {
       TUINavigationRow(
         title = "Label",
         leadingIcon = TarkaIcons.Copy,
-        badgeCount = 5,
         onClick = onClick,
-        badgeTestTag = BADGE_TAG,
         rowTestTag = ROW_TAG
-      )
+      ){
+
+      }
     }
     composable.onNodeWithTag(ROW_TAG).performClick()
-
     verify(onClick).invoke()
 
     composable.onNodeWithTag(ROW_TAG)
--- tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUINavigationRows.kt
+++ tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUINavigationRows.kt
@@ -1,10 +1,14 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
 
 package com.tarkalabs.uicomponents.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,83 +18,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tarkalabs.uicomponents.Tags
 import com.tarkalabs.uicomponents.models.TarkaIcon
-import com.tarkalabs.uicomponents.models.TarkaIcons
-import com.tarkalabs.uicomponents.theme.TUIColors
 import com.tarkalabs.uicomponents.theme.TUITheme
 
 // TODO: 5/2/2023 use dimensions instead of hard coded things
-
-/**
- *
- * The TUINavigationRow composable function creates a row that represents a navigation item in a UI.
- * It handles click events and provides a callback for customization. The row can display a title,
- * an optional leading icon, a badge count, and a right arrow icon. The appearance can be modified
- * using the provided modifiers. When the row is clicked, the specified [onClick] function is invoked.
- *
- * @param title: The title text to be displayed in the navigation row.
- * @param leadingIcon: The optional leading icon displayed before the title.
- * @param badgeCount: The optional count to display as a badge.
- * @param showRightArrow: Whether to show a right arrow icon.
- * @param modifier: The modifier to apply to the row.
- * @param badgeTestTag: The test tag for the badge.
- * @param rowTestTag: The test tag for the row.
- * @param onClick: The callback function when the row is clicked.
- *
- * Example usage:
- *
- * TUINavigationRow(
- *   title = "Profile",
- *   leadingIcon = TarkaIcons.Profile,
- *   badgeCount = 2,
- *   showRightArrow = true,
- *   modifier = Modifier.fillMaxWidth(),
- *   badgeTestTag = "profile_badge",
- *   rowTestTag = "profile_row",
- *   onClick = { /* Handle click event */ }
- * )
- *
- */
-@OptIn(ExperimentalMaterial3Api::class) @Composable fun TUINavigationRow(
-  title: String,
-  leadingIcon: TarkaIcon? = null,
-  badgeCount: Int? = null,
-  showRightArrow: Boolean = false,
-  modifier: Modifier = Modifier,
-  badgeTestTag: String = Tags.TAG_NAVIGATION_ROW_BADGE,
-  rowTestTag: String = Tags.TAG_NAVIGATION_ROW,
-  onClick: () -> Unit,
-) {
-  TUINavigationRow(
-    title = title,
-    onClick = onClick,
-    leadingIcon = leadingIcon,
-    rowTestTag = rowTestTag,
-    modifier = modifier
-  ) {
-    if (badgeCount != null) Badge(
-      containerColor = TUITheme.colors.error,
-      modifier = Modifier
-        .size(18.dp)
-        .align(Alignment.CenterVertically)
-        .testTag(badgeTestTag),
-    ) {
-      Text(
-        text = badgeCount.toString(),
-        style = TUITheme.typography.button8,
-        color = TUITheme.colors.onTertiary,
-      )
-    }
-
-    if (showRightArrow) {
-      Icon(
-        painter = painterResource(id = TarkaIcons.ChevronRight.iconRes),
-        contentDescription = TarkaIcons.ChevronRight.contentDescription,
-        tint = TUITheme.colors.utilityOutline
-      )
-    }
-  }
-}
-
 /**
  *
  * The TUINavigationRow composable function creates a row that represents a navigation item in a UI.
