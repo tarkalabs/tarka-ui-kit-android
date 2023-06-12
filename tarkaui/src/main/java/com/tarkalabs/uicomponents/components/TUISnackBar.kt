4ded33a77bfd304ffdb841f1af65ad6a2a6b65cc
AskNilesh <rathodnilsrk@gmail.com>
2023-06-12 16:31:10 +0530

    - Created enum class for Snackbar type

--- tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUISnackBar.kt
+++ tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUISnackBar.kt
@@ -1,15 +1,51 @@
 package com.tarkalabs.uicomponents.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.tarkalabs.uicomponents.components.TUISnackBarType.Error
+import com.tarkalabs.uicomponents.components.TUISnackBarType.Information
+import com.tarkalabs.uicomponents.components.TUISnackBarType.Success
+import com.tarkalabs.uicomponents.components.TUISnackBarType.Warning
+import com.tarkalabs.uicomponents.models.TarkaIcon
+import com.tarkalabs.uicomponents.theme.TUITheme
 
-enum class TUISnackBarType{
+enum class TUISnackBarType {
   Success,
   Information,
   Warning,
   Error;
 }
 
-@Composable
-fun TUISnackBar() {
+@Composable fun TUISnackBar(
+  message: String,
+  type: TUISnackBarType = TUISnackBarType.Information,
+  leadingIcon: TarkaIcon? = null,
+  actionLabel: String? = null,
+  action: () -> Unit = {}
+) {
+  var containerColor = TUITheme.colors.onSecondary
+  when(type){
+    Success -> TODO()
+    Information -> TODO()
+    Warning -> TODO()
+    Error -> TODO()
+  }
 
+  Snackbar(containerColor = TUITheme.colors.utilityOutline) {
+    Row {
+      if (leadingIcon != null) {
+        Icon(
+          painterResource(id = leadingIcon.iconRes),
+          contentDescription = leadingIcon.contentDescription
+        )
+      }
+
+      Text(message)
+    }
+
+  }
 }
