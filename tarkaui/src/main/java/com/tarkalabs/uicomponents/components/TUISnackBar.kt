bd415b4d86a0a20fa7cb852293352c594eeb544e
AskNilesh <rathodnilsrk@gmail.com>
2023-06-12 15:52:34 +0530

    Created TUISnackBar

---
+++ tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUISnackBar.kt
@@ -0,0 +1,15 @@
+package com.tarkalabs.uicomponents.components
+
+import androidx.compose.runtime.Composable
+
+enum class TUISnackBarType{
+  Success,
+  Information,
+  Warning,
+  Error;
+}
+
+@Composable
+fun TUISnackBar() {
+
+}
