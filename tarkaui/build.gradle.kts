b385f2efa681c61ed14925dddbb9aaab0eb0999a
AskNilesh <rathodnilsrk@gmail.com>
2023-07-04 11:29:35 +0530

    - Version upgrade

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "0.17-alpha"
+        version = "0.18-alpha"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
