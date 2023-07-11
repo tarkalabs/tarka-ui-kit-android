638b954ec3a07a1f2a3c76ddfdcbe5d60c24848f
AskNilesh <rathodnilsrk@gmail.com>
2023-07-11 21:54:39 +0530

    - UI test for TUIEmailFieldTest click events

--- tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/TUIEmailFieldTest.kt
+++ tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/TUIEmailFieldTest.kt
@@ -6,15 +6,21 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performTextInput
 import com.tarkalabs.uicomponents.components.TUIEmailField
 import com.tarkalabs.uicomponents.components.TUIEmailFieldTags
+import com.tarkalabs.uicomponents.components.TUIIconButtonTags
 import com.tarkalabs.uicomponents.models.TarkaIcons
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
 class TUIEmailFieldTest {
   @get:Rule val composable = createComposeRule()
-  private val tags: TUIEmailFieldTags = TUIEmailFieldTags()
+  private val tags: TUIEmailFieldTags = TUIEmailFieldTags(iconButtonTag = TUIIconButtonTags(parentTag = "TRAILING_ICON"))
   private val emailList = listOf(  "mike32@soft.com",
     "mike.smith@corp.co",
     "mike32@soft.com",)
@@ -25,7 +31,7 @@ class TUIEmailFieldTest {
         mutableStateListOf(
           "mike32@soft.com",
           "mike.smith@corp.co",
-          "mike32@soft.com",
+          "mike33@soft.com",
         )
       }
       TUIEmailField(
@@ -50,9 +56,49 @@ class TUIEmailFieldTest {
     composable.onNodeWithTag(tags.textFieldTag, useUnmergedTree = true).assertIsDisplayed()
     composable.onNodeWithTag(tags.flowRowTag, useUnmergedTree = true).assertIsDisplayed()
     composable.onNodeWithText("To", useUnmergedTree = true).assertIsDisplayed()
-    // emailList.forEach { email ->
-    //   composable.onNodeWithText(email, useUnmergedTree = true).assertIsDisplayed()
-    // }
     composable.onNodeWithTag(tags.iconButtonTag.parentTag, useUnmergedTree = true).assertIsDisplayed()
+    emailList.forEach { email ->
+      composable.onNodeWithText(email, useUnmergedTree = true).assertIsDisplayed()
+    }
   }
+
+  @Test
+  fun click_event_is_Triggered(){
+    val trailingIconClick: () -> Unit = mock()
+
+    composable.setContent {
+      val emailList = remember {
+        mutableStateListOf(
+          "mike32@soft.com",
+          "mike.smith@corp.co",
+          "mike33@soft.com",
+        )
+      }
+      TUIEmailField(
+        title = "To",
+        emailAddressList = emailList,
+        trailingIcon = TarkaIcons.AddCircle24Regular,
+        onItemRemoved = { position ->
+
+        },
+        trailingIconClick = trailingIconClick,
+        onItemAdd = {
+          emailList.add(it)
+        },
+        tags = tags
+      )
+    }
+
+    composable.onNodeWithTag(tags.iconButtonTag.parentTag).performClick()
+    verify(trailingIconClick).invoke()
+
+    val inputText = "android@gmail.com"
+    val textField = composable.onNodeWithTag(tags.textFieldTag, useUnmergedTree = true)
+    textField .performTextInput(inputText)
+    textField.performImeAction()
+
+    composable.onNodeWithText(inputText, useUnmergedTree = true).assertIsDisplayed()
+
+  }
+
 }
