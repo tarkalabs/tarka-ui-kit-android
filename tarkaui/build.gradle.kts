48e3d61f22eb3309dd8f5a120b152bd63a060d73
Raja Jawahar <rajajawahar77@gmail.com>
2023-07-18 17:22:58 +0530

    Version bumped to 0.25-alphs

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "0.24-alpha"
+        version = "0.25-alpha"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
