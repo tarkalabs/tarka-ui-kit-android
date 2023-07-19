f1cd29958a5989f809de653b5484ade541258ff4
Raja Jawahar <rajajawahar77@gmail.com>
2023-07-19 11:26:09 +0530

    Version updated to 0.27-alpha

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "0.26-alpha"
+        version = "0.27-alpha"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
