4d5992b2744fdddcc02ab4afc516a9bc32fe5d28
Younes Charfaoui <mxcsyounes@gmail.com>
2023-08-16 15:31:42 +0100

    Fixing the issue of api transient

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
   implementation("androidx.compose.material3:material3:1.1.0-rc01")
   implementation("androidx.compose.foundation:foundation:$composeUiVersion")
-  implementation(project(":tarkaicons"))
+  api(project(":tarkaicons"))
 
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
