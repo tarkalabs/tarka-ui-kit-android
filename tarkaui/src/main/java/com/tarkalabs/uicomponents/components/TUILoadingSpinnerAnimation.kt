27264a234e88f489efbb8cf272ef271a047f77ce
Mahendran <Mahendran@eam360.com>
2023-08-17 19:37:08 +0530

    Comments & TestTags were included in TUILoadingSpinnerAnimation

--- tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUILoadingSpinnerAnimation.kt
+++ tarkaui/src/main/java/com/tarkalabs/uicomponents/components/TUILoadingSpinnerAnimation.kt
@@ -23,20 +23,54 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tarkalabs.uicomponents.R
 import com.tarkalabs.uicomponents.theme.TUITheme
 
+/**
+ * This Composable function used as a content when the UI is ready to show the ProgressBar layout
+ * to show something is Loading in the background and intimates the user to wait
+ *
+ * This composable function simply contains
+ * @param modifier to modify the properties of the parent component - Box
+ * @param tags tags to used while testing to pick the particular component used inside this component.
+ *
+ * CustomProgressIndicator Composable function uses the canvas API in jetpack compose to the
+ * The Custom Circular Progressbar with below steps.
+ *
+ * 1) one White Circle was drawn in the screen.
+ * 2) one arc was drawn in the quarter size of above circle with proper angle
+ * to overlap fine with circle.
+ * 3) the Rotation Animation was performed.
+ *
+ * val circleRadius = radius - strokeWidth / 2
+ *
+ * here while drawing circle in canvas with stroke.
+ * the stroke width is drawn partially inside of the outline of the circle & outside of the circle.
+ * so in order to find the correct radius of the circle we are subtracting radius by the half of strokeWidth drawn in canvas.
+ *
+ *  val arcStartX = centerX - circleRadius
+ *  val arcStartY = centerY - circleRadius
+ *
+ *  similarly the arc's offset also calculated by subtracting the center of circle by the (circleRadius)
+ *  which is explained in above paragraph to position the arc in a right way above the circle
+ *  otherwise the arc will be deviated from circle.
+ * */
 @Composable
 fun TUILoadingSpinnerAnimation(
   modifier: Modifier = Modifier,
+  tags: TUILoadingSpinnerAnimationTags = TUILoadingSpinnerAnimationTags(),
 ) {
-  Box(modifier = modifier, contentAlignment = Alignment.Center) {
-    CustomProgressIndicator(modifier = Modifier.size(76.dp))
+  Box(modifier = modifier.testTag(tags.parentTag), contentAlignment = Alignment.Center) {
+    CustomProgressIndicator(modifier = Modifier
+      .size(76.dp)
+      .testTag(tags.progressBarTag))
     Image(
       modifier = Modifier
+        .testTag(tags.loaderImageTag)
         .width(60.dp)
         .height(11.dp),
       painter = painterResource(id = R.drawable.loader_spin_image),
@@ -94,6 +128,12 @@ fun TUILoadingSpinnerAnimation(
     )
   }
 }
+
+data class TUILoadingSpinnerAnimationTags(
+  val parentTag: String = "TUILoadingSpinnerAnimationTag",
+  val progressBarTag: String = "progressBarTag",
+  val loaderImageTag: String = "EamImageTag",
+)
 
 @Preview
 @Composable
