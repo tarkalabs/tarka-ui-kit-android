ca570e97688e04e369f1bd4644be4a280d0b971f
AskNilesh <rathodnilsrk@gmail.com>
2023-10-05 11:34:15 +0530

    - added one more variant to election Card

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "0.47-alpha"
+        version = "0.48-alpha"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
