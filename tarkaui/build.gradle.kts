04f00e3b403df974dc33ac46804050d7fa4df33c
AskNilesh <rathodnilsrk@gmail.com>
2023-11-08 12:34:17 +0530

    - Fixed UI issue in TUIChip

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -64,7 +64,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "0.54-alpha"
+        version = "0.55-alpha"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
