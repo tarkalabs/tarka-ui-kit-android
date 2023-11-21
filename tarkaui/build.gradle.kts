1ad805c77637a96af6cf6d3f00f6f702ef749231
Mahendran <Mahendran@eam360.com>
2023-11-21 17:33:48 +0530

    Project version increased

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "0.52-alpha"
+        version = "0.53-alpha"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
