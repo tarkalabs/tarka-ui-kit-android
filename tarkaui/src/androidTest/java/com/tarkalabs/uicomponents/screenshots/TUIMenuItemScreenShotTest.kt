f72f20154134185886b2add4ef28be0bd3648aa7
Raja Jawahar <rajajawahar77@gmail.com>
2024-01-30 13:51:12 +0530

    Support fucntion added for Screeshot teseting

--- tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/screenshots/TUIMenuItemScreenShotTest.kt
+++ tarkaui/src/androidTest/java/com/tarkalabs/uicomponents/screenshots/TUIMenuItemScreenShotTest.kt
@@ -1,9 +1,14 @@
 package com.tarkalabs.uicomponents.screenshots
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
+import com.tarkalabs.tarkaicons.Checkmark24
+import com.tarkalabs.tarkaicons.Circle24
+import com.tarkalabs.tarkaicons.TarkaIcons.Regular
 import com.tarkalabs.uicomponents.components.TUIMenuItem
 import com.tarkalabs.uicomponents.components.TUIMenuItemIconStyle
 import com.tarkalabs.uicomponents.components.TUIMenuItemIconStyle.SUCCESS
+import com.tarkalabs.uicomponents.components.TUIMenuItemTags
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -52,7 +57,26 @@ class TUIMenuItemScreenShotTest(
         label = "Label",
         isSelected = isSelected,
         onMenuItemClick = { /*TODO*/ },
-        leadingIconStyle = leadingIconStyle
+        leadingIconStyle = leadingIconStyle,
+        leadingIcon = if (leadingIconStyle == SUCCESS) Regular.Checkmark24 else null
+      )
+    }
+  }
+
+  @Test
+  fun testTuiMenuItem_Failure_With_Leading_Icon_Cirlce() {
+    compareScreenshotFor(
+      darkTheme,
+      "Custom_LeadingIconStyle_Failure_isSelected_false_darkTheme_${darkTheme}"
+    ) {
+      TUIMenuItem(
+        modifier = Modifier.fillMaxWidth(),
+        label = "Label",
+        isSelected = false,
+        leadingIcon = Regular.Circle24,
+        trailingIcon = Regular.Circle24,
+        onMenuItemClick = {},
+        tags = TUIMenuItemTags(parentTag = "hello")
       )
     }
   }
