f6cd485e85fdf822a3e61e0fca34384ac3ebea13
Megna <smeg9793@gmail.com>
2024-01-31 17:07:27 +0530

    version upgraded

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -64,7 +64,7 @@ publishing {
       run {
         groupId = "com.tarkalabs"
         artifactId = getLibraryArtifactId()
-        version = "1.0.3"
+        version = "1.0.4"
         artifact("$buildDir/outputs/aar/${getLibraryArtifactId()}-release.aar")
       }
     }
