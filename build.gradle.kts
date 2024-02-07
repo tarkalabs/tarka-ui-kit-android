4b83f2216d90e61389b2676a2d979ce08cf50e99
Raja Jawahar <rajajawahar77@gmail.com>
2024-02-07 21:23:54 +0530

    Small fix in package naming

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,5 @@ plugins {
   id("com.android.library") version "7.4.2" apply false
   id("org.jetbrains.kotlin.android") version "1.8.10" apply false
   id("com.vanniktech.maven.publish") version "0.27.0"
-  id ("org.jetbrains.dokka' version") version  "1.8.10" apply false
+  id("org.jetbrains.dokka") version  "1.8.10" apply false
 }
