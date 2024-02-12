bc0dd7db747e85e3ab5a31c0f7429fa42bee7f3c
Mahendran <Mahendran@eam360.com>
2024-02-12 16:49:42 +0530

    TUIInspectionModule removed here & moved to eam-360 ui repo

--- tarkaui/build.gradle.kts
+++ tarkaui/build.gradle.kts
@@ -95,7 +95,6 @@ dependencies {
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
   androidTestImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-  androidTestImplementation("org.mockito:mockito-android:4.5.1")
 
   androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
   androidTestImplementation("org.mockito:mockito-android:4.5.1")
