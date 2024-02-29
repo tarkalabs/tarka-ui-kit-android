acf14623e00e3fc453ee0e1a7d63f369e4fc2ed3
Raja Jawahar <rajajawahar77@gmail.com>
2024-02-29 10:03:17 +0530

    Compilation issue fixes

--- example/src/main/java/com/tarkalabs/ui/UIComponentListActivity.kt
+++ example/src/main/java/com/tarkalabs/ui/UIComponentListActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,21 +23,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.tarkalabs.tarkaui.components.TUIAppTopBar
-import com.tarkalabs.tarkaui.components.TUISearchBar
-import com.tarkalabs.tarkaui.icons.BarcodeScanner24
-import com.tarkalabs.tarkaui.icons.ChevronRight20
-import com.tarkalabs.tarkaui.icons.Dismiss16
+import com.tarkalabs.tarkaui.icons.ChevronLeft24
 import com.tarkalabs.tarkaui.icons.TarkaIcons
-import com.tarkalabs.tarkaui.icons.TarkaIcons.Filled
 import com.tarkalabs.tarkaui.icons.TarkaIcons.Regular
 import com.tarkalabs.tarkaui.theme.TUITheme
 
@@ -59,7 +52,7 @@ class UIComponentListActivity : ComponentActivity() {
         Scaffold(topBar = {
           TUIAppTopBar(
             title = title,
-            navigationIcon = if (isInComponent) Regular.ChevronLeft24 else Regular.Stack24,
+            navigationIcon = if (isInComponent) TarkaIcons.Regular.ChevronLeft24 else Regular.Stack24,
             onNavigationIconClick = {
               if (isInComponent) {
                 navController.popBackStack()
@@ -264,8 +257,6 @@ val componentList = listOf(
   "TUICheckBox",
   "TUIRadioButton",
   "TUIRadioButtonRow",
-  "TUIFloatingNavBar",
-  "TUIFloatingTab",
   "TUITab",
   "TUITabBar",
   "TUIAttachmentUpload",
@@ -283,6 +274,5 @@ val componentList = listOf(
   "TUISelectionCard",
   "TUITag",
   "TUIPlayPauseButton",
-  "TUIMultiLevelSelectorHeader"
 )
 
