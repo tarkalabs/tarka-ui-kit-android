2827517963906c2657b39c42c9418b494ae69b86
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:28:50 +0530

    - Fixed imports issue in TUITabComposable

--- example/src/main/java/com/tarkalabs/ui/TUITabComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUITabComposable.kt
@@ -4,34 +4,33 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.tarkaicons.Send20
-import com.tarkalabs.tarkaicons.TarkaIcons
-import com.tarkalabs.uicomponents.components.tab.TUITab
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.tab.TUITab
+import com.tarkalabs.tarkaui.icons.Send20
+import com.tarkalabs.tarkaui.icons.TarkaIcons
+import com.tarkalabs.tarkaui.theme.TUITheme
 
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TUITabComposable() {
-    Column(Modifier.fillMaxSize(). padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "TUITab", style = TUITheme.typography.heading3)
-        // Example Tab with default appearance
-        TUITab(
-            modifier = Modifier
-                .width(105.dp)
-                .padding(8.dp),
-            title = "Tab 1",
-            isSelected = true,
-            leadingIcon = TarkaIcons.Regular.Send20,
-            onTabClicked = {
-                // Handle tab click
-            },
-        )
-    }
+@Composable fun TUITabComposable() {
+  Column(
+      Modifier
+          .fillMaxSize()
+          .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Text(text = "TUITab", style = TUITheme.typography.heading3)
+    // Example Tab with default appearance
+    TUITab(
+      modifier = Modifier
+          .width(105.dp)
+          .padding(8.dp),
+      title = "Tab 1",
+      isSelected = true,
+      leadingIcon = TarkaIcons.Regular.Send20,
+      onTabClicked = {
+        // Handle tab click
+      },
+    )
+  }
 }
