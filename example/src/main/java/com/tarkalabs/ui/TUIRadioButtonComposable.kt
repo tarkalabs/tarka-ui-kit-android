606f0909accd1b9f96a50d53818d404c95aa9aa7
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:24:01 +0530

    - Fixed imports issue in TUIRadioButtonComposable

--- example/src/main/java/com/tarkalabs/ui/TUIRadioButtonComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUIRadioButtonComposable.kt
@@ -3,36 +3,31 @@ package com.tarkalabs.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.uicomponents.components.radiobutton.TUIRadioButton
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.radiobutton.TUIRadioButton
+import com.tarkalabs.tarkaui.theme.TUITheme
 
+@Composable fun TUIRadioButtonComposable() {
+  Column(
+      Modifier
+          .fillMaxSize()
+          .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Text(text = "TUIRadioButton", style = TUITheme.typography.heading3)
+    TUIRadioButton(
+      modifier = Modifier.padding(8.dp),
+      selected = true,
+      onOptionSelected = {},
+    )
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TUIRadioButtonComposable() {
-    Column(Modifier.fillMaxSize(). padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "TUIRadioButton", style = TUITheme.typography.heading3)
-        TUIRadioButton(
-            modifier = Modifier
-                .padding(8.dp),
-            selected = true,
-            onOptionSelected = {
-            },
-        )
-
-        TUIRadioButton(
-            modifier = Modifier
-                .padding(8.dp),
-            selected = false,
-            enabled = false,
-            onOptionSelected = {
-            },
-        )
-    }
+    TUIRadioButton(
+      modifier = Modifier.padding(8.dp),
+      selected = false,
+      enabled = false,
+      onOptionSelected = {},
+    )
+  }
 }
