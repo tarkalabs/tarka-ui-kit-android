e8459ff1e0feedc52d85f3cc8ceb0eb42560c687
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:00:29 +0530

    - Fixed imports issue in TUIBadgeComposable

--- example/src/main/java/com/tarkalabs/ui/TUIBadgeComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUIBadgeComposable.kt
@@ -6,19 +6,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.uicomponents.components.base.BadgeSize
-import com.tarkalabs.uicomponents.components.base.TUIBadge
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.base.BadgeSize
+import com.tarkalabs.tarkaui.components.base.TUIBadge
+import com.tarkalabs.tarkaui.theme.TUITheme
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TUIBadgeComposable() {
     Column(
