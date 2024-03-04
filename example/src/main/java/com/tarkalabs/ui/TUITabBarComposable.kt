dab06efbafeb36d960b6e60d0279e93abdcdc8d3
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:28:19 +0530

    - Fixed imports issue in TUITabBarComposable

--- example/src/main/java/com/tarkalabs/ui/TUITabBarComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUITabBarComposable.kt
@@ -4,38 +4,35 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.tarkaicons.Circle12
-import com.tarkalabs.tarkaicons.TarkaIcons.Regular
-import com.tarkalabs.uicomponents.components.tab.TUITabBar
-import com.tarkalabs.uicomponents.components.tab.TabItem
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.tab.TUITabBar
+import com.tarkalabs.tarkaui.components.tab.TabItem
+import com.tarkalabs.tarkaui.icons.Circle12
+import com.tarkalabs.tarkaui.icons.TarkaIcons.Regular
+import com.tarkalabs.tarkaui.theme.TUITheme
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TUITabBarComposable() {
+@Composable fun TUITabBarComposable() {
   Column(
-      Modifier
-          .fillMaxSize()
-          .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Modifier
+      .fillMaxSize()
+      .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally
+  ) {
     Text(text = "TUITabBar", style = TUITheme.typography.heading3)
     TUITabBar(
       modifier = Modifier
-          .fillMaxWidth()
-          .padding(8.dp),
+        .fillMaxWidth()
+        .padding(8.dp),
       tabItems = listOf(
         TabItem("Tab", leadingTabIcon = Regular.Circle12),
         TabItem("Tab", leadingTabIcon = Regular.Circle12),
         TabItem("Tab", leadingTabIcon = Regular.Circle12)
       ),
       selectedTabIndex = 0,
-      onTabChanged = {
-      },
+      onTabChanged = {},
     )
   }
 }
