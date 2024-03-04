9c6ae072f0f015bf8667e5395a98fb9b0803e663
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:30:51 +0530

    - Fixed imports issue in TUIToggleRowComposable

--- example/src/main/java/com/tarkalabs/ui/TUIToggleRowComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUIToggleRowComposable.kt
@@ -3,30 +3,28 @@ package com.tarkalabs.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.uicomponents.components.base.TUIToggleRow
-import com.tarkalabs.uicomponents.components.base.ToggleRowStyle
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.base.TUIToggleRow
+import com.tarkalabs.tarkaui.components.base.ToggleRowStyle
+import com.tarkalabs.tarkaui.theme.TUITheme
 
+@Composable fun TUIToggleRowComposable() {
+  Column(
+    Modifier
+      .fillMaxSize()
+      .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Text(text = "TUIToggleRow", style = TUITheme.typography.heading3)
+    TUIToggleRow(
+      title = "Title", style = ToggleRowStyle.Title
+    )
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TUIToggleRowComposable() {
-    Column(Modifier.fillMaxSize(). padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "TUIToggleRow", style = TUITheme.typography.heading3)
-        TUIToggleRow(
-            title = "Title",
-            style = ToggleRowStyle.Title
-        )
-
-        TUIToggleRow(
-            title = "Title",
-            style = ToggleRowStyle.TitleWithDescription("Description")
-        )
-    }
+    TUIToggleRow(
+      title = "Title", style = ToggleRowStyle.TitleWithDescription("Description")
+    )
+  }
 }
