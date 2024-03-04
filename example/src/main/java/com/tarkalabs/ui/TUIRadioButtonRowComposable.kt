10241e0d41901505ae4fd3790d0edca036d36317
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:24:27 +0530

    - Fixed imports issue in TUIRadioButtonRowComposable

--- example/src/main/java/com/tarkalabs/ui/TUIRadioButtonRowComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUIRadioButtonRowComposable.kt
@@ -3,16 +3,13 @@ package com.tarkalabs.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.theme.TUITheme
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TUIRadioButtonRowComposable() {
     Column(Modifier.fillMaxSize(). padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
