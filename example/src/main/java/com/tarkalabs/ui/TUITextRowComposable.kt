681d30873709255694cb18173501f3bf23a11870
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 18:30:12 +0530

    - Fixed imports issue in TUITextRowComposable

--- example/src/main/java/com/tarkalabs/ui/TUITextRowComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUITextRowComposable.kt
@@ -3,32 +3,29 @@ package com.tarkalabs.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.tarkaicons.ChevronRight20
-import com.tarkalabs.tarkaicons.TarkaIcons
-import com.tarkalabs.uicomponents.components.TUITextRow
-import com.tarkalabs.uicomponents.components.TextRowStyle.Title
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.TUITextRow
+import com.tarkalabs.tarkaui.components.TextRowStyle.Title
+import com.tarkalabs.tarkaui.icons.ChevronRight20
+import com.tarkalabs.tarkaui.icons.TarkaIcons
+import com.tarkalabs.tarkaui.theme.TUITheme
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TUITextRowComposable() {
+@Composable fun TUITextRowComposable() {
   Column(
-      Modifier
-          .fillMaxSize()
-          .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Modifier
+      .fillMaxSize()
+      .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally
+  ) {
     Text(text = "TUITextRow", style = TUITheme.typography.heading3)
 
     TUITextRow(title = "Title",
       style = Title,
       infoIcon = TarkaIcons.Regular.ChevronRight20,
       onTextRowClick = { /* Handle text row click */ },
-      onInfoIconClick = { /* Handle info icon click */ }
-    )
+      onInfoIconClick = { /* Handle info icon click */ })
   }
 }
