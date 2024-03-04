2d38130b41ed7c4744e4f1e1ab4193a2d5ca8526
AskNilesh <rathodnilsrk@gmail.com>
2024-03-04 17:59:35 +0530

    - Fixed imports issue in TUIAvatarComposable

--- example/src/main/java/com/tarkalabs/ui/TUIAvatarComposable.kt
+++ example/src/main/java/com/tarkalabs/ui/TUIAvatarComposable.kt
@@ -4,33 +4,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.tarkalabs.tarkaicons.Person16
-import com.tarkalabs.tarkaicons.TarkaIcons
-import com.tarkalabs.uicomponents.components.base.AvatarSize
-import com.tarkalabs.uicomponents.components.base.AvatarType
-import com.tarkalabs.uicomponents.components.base.TUIAvatar
-import com.tarkalabs.uicomponents.theme.TUITheme
+import com.tarkalabs.tarkaui.components.base.AvatarSize
+import com.tarkalabs.tarkaui.components.base.AvatarType
+import com.tarkalabs.tarkaui.components.base.TUIAvatar
+import com.tarkalabs.tarkaui.icons.Person16
+import com.tarkalabs.tarkaui.icons.TarkaIcons
+import com.tarkalabs.tarkaui.theme.TUITheme
 
+@Composable fun TUIAvatarComposable() {
+  Column(
+    Modifier
+      .fillMaxSize()
+      .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Text(text = "TUIAvatar", style = TUITheme.typography.heading3)
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TUIAvatarComposable() {
-    Column(Modifier.fillMaxSize(). padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "TUIAvatar", style = TUITheme.typography.heading3)
-
-        TUIAvatar(
-            modifier = Modifier.size(100.dp),
-            avatarType = AvatarType.Icon(TarkaIcons.Regular.Person16),
-            avatarSize = AvatarSize.M,
-            showBadge = true
-        )
-    }
+    TUIAvatar(
+      modifier = Modifier.size(100.dp),
+      avatarType = AvatarType.Icon(TarkaIcons.Regular.Person16),
+      avatarSize = AvatarSize.M,
+      showBadge = true
+    )
+  }
 }
