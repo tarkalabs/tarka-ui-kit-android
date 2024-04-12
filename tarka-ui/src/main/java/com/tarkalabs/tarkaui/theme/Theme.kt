150aba1cf2052787176e2171a2d654ff1fff9fe0
Mohanraj293 <mohanrajrengarajr@gmail.com>
2024-04-12 11:44:24 +0530

    Statusbar color updated based on theme.

--- tarka-ui/src/main/java/com/tarkalabs/tarkaui/theme/Theme.kt
+++ tarka-ui/src/main/java/com/tarkalabs/tarkaui/theme/Theme.kt
@@ -1,9 +1,13 @@
 package com.tarkalabs.tarkaui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 
 @Composable fun TUITheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
@@ -12,6 +16,14 @@ import androidx.compose.runtime.CompositionLocalProvider
   content: @Composable () -> Unit,
 ) {
   val colors = if (darkTheme) darkColors else lightColors
+  val view = LocalView.current
+
+  if (!view.isInEditMode) {
+    SideEffect {
+      val window = (view.context as Activity).window
+      window.statusBarColor = colors.surface.toArgb()
+    }
+  }
 
   CompositionLocalProvider(
     LocalTUITypography provides extendedTypography, LocalTUIColors provides colors
