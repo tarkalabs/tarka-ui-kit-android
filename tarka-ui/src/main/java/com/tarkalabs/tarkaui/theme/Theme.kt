d5e251bd6f118e139d6da4c2ee0efe66e7c0af9c
Mohanraj293 <mohanrajrengarajr@gmail.com>
2024-04-12 18:53:48 +0530

    Minor upgrade.

--- tarka-ui/src/main/java/com/tarkalabs/tarkaui/theme/Theme.kt
+++ tarka-ui/src/main/java/com/tarkalabs/tarkaui/theme/Theme.kt
@@ -1,13 +1,18 @@
 package com.tarkalabs.tarkaui.theme
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable fun TUITheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
@@ -22,6 +27,7 @@ import androidx.compose.ui.platform.LocalView
     SideEffect {
       val window = (view.context as Activity).window
       window.statusBarColor = colors.surface.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
   }
 
