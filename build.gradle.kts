53493a3c5842221b3b6a551d6538ac534ca4148c
Younes Charfaoui <mxcsyounes@gmail.com>
2024-05-07 10:12:21 +0100

    Adding Compose Compiler Reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,22 @@ plugins {
   id("com.vanniktech.maven.publish") version "0.27.0"
   id("org.jetbrains.dokka") version  "1.8.10" apply false
 }
+
+subprojects {
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+      if (project.findProperty("composeCompilerReports") == "true") {
+        freeCompilerArgs += listOf(
+          "-P",
+          "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
+        )
+      }
+      if (project.findProperty("composeCompilerMetrics") == "true") {
+        freeCompilerArgs += listOf(
+          "-P",
+          "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
+        )
+      }
+    }
+  }
+}
