7cce56c36bde802dd39d902508f495a7bdcf4d92
priyacs651 <priyacs651@gmail.com>
2024-05-17 23:23:36 +0530

    Implemented TUIPopUpMenu in HeaderWithTrailingIcon

--- tarka-ui/src/main/java/com/tarkalabs/tarkaui/components/TUIMobileOverlayHeader.kt
+++ tarka-ui/src/main/java/com/tarkalabs/tarkaui/components/TUIMobileOverlayHeader.kt
@@ -5,21 +5,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tarkalabs.tarkaui.components.MobileOverlayMenuItemLeadingContentType.Icon
+import com.tarkalabs.tarkaui.components.MobileOverlayMenuItemStyle.Title
 import com.tarkalabs.tarkaui.components.TUIMobileOverlayHeaderStyle.HeaderWithBackIcon
 import com.tarkalabs.tarkaui.components.TUIMobileOverlayHeaderStyle.HeaderWithTitle
 import com.tarkalabs.tarkaui.components.TUIMobileOverlayHeaderStyle.HeaderWithTrailingIcon
@@ -29,7 +39,7 @@ import com.tarkalabs.tarkaui.components.base.IconButtonStyle
 import com.tarkalabs.tarkaui.components.base.TUIIconButton
 import com.tarkalabs.tarkaui.components.base.TUIIconButtonTags
 import com.tarkalabs.tarkaui.icons.ChevronLeft24
-import com.tarkalabs.tarkaui.icons.Dismiss24
+import com.tarkalabs.tarkaui.icons.MoreHorizontal24
 import com.tarkalabs.tarkaui.icons.TarkaIcon
 import com.tarkalabs.tarkaui.icons.TarkaIcons
 import com.tarkalabs.tarkaui.theme.TUITheme
@@ -61,7 +71,9 @@ sealed class TUIMobileOverlayHeaderStyle {
   data class HeaderWithTrailingIcon(
     val title: String,
     val trailingIcon: TarkaIcon,
-    val onTrailingIconClick: () -> Unit
+    val onTrailingIconClick: () -> Unit,
+    val menuItemList: List<TUIPopUpMenu>? = null,
+    val onMenuItemClick: ((TUIPopUpMenu) -> Unit)? = null,
   ) : TUIMobileOverlayHeaderStyle()
 
   /**
@@ -71,8 +83,7 @@ sealed class TUIMobileOverlayHeaderStyle {
    * @property onBackIconClick The click listener for the back icon.
    */
   data class HeaderWithBackIcon(
-    val title: String,
-    val onBackIconClick: () -> Unit
+    val title: String, val onBackIconClick: () -> Unit
   ) : TUIMobileOverlayHeaderStyle()
 }
 
@@ -87,13 +98,14 @@ sealed class TUIMobileOverlayHeaderStyle {
 fun TUIMobileOverlayHeader(
   modifier: Modifier = Modifier,
   style: TUIMobileOverlayHeaderStyle,
-  tags : TUIMobileOverlayHeaderTags = TUIMobileOverlayHeaderTags()
+  tags: TUIMobileOverlayHeaderTags = TUIMobileOverlayHeaderTags()
 ) {
 
   val height = when (style) {
     is None -> 24.dp
     else -> 64.dp
   }
+  var expanded by remember { mutableStateOf(false) }
 
   Box(
     modifier = modifier
@@ -101,7 +113,11 @@ fun TUIMobileOverlayHeader(
       .fillMaxWidth()
       .testTag(tags.parentTag)
   ) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center) {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier.fillMaxWidth(),
+      verticalArrangement = Arrangement.Center
+    ) {
       Box(
         modifier = Modifier
           .padding(vertical = 8.dp)
@@ -132,42 +148,80 @@ fun TUIMobileOverlayHeader(
           }
 
           is HeaderWithTitle -> {
-            HeaderText(title = style.title, TextAlign.Center, modifier = Modifier.weight(1f))
+            HeaderText(
+              title = style.title,
+              TextAlign.Center,
+              modifier = Modifier.weight(1f)
+            )
           }
 
           is HeaderWithTrailingIcon -> {
             HeaderText(
-              title = style.title, textAlign = TextAlign.Start, modifier = Modifier
+              title = style.title,
+              textAlign = TextAlign.Start,
+              modifier = Modifier
                 .weight(1f)
                 .padding(start = 16.dp)
 
             )
-            TUIIconButton(
-              icon = style.trailingIcon,
-              buttonSize = IconButtonSize.L,
-              iconButtonStyle = IconButtonStyle.Ghost,
-              onIconClick = style.onTrailingIconClick,
-              tags = tags.trailingIconButtonTag
-            )
+            Box(modifier = Modifier.wrapContentSize(Alignment.BottomEnd)) {
+              TUIIconButton(
+                icon = style.trailingIcon,
+                buttonSize = IconButtonSize.L,
+                iconButtonStyle = IconButtonStyle.Ghost,
+                onIconClick = {
+                  if (!style.menuItemList.isNullOrEmpty()) {
+                    expanded = !expanded
+                  } else {
+                    style.onTrailingIconClick.invoke()
+                  }
+                },
+                tags = tags.trailingIconButtonTag
+              )
+              DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                  .background(TUITheme.colors.surface)
+                  .align(Alignment.TopEnd),
+              ) {
+                style.menuItemList?.forEach { item ->
+                  TUIMobileOverlayMenuItem(
+                    title = stringResource(id = item.title),
+                    isSelected = false,
+                    style = Title,
+                    onMobileOverlayMenuItemClick = {
+                      expanded = false
+                      style.onMenuItemClick?.invoke(item)
+                    },
+                    modifier = Modifier.defaultMinSize(minWidth = 160.dp),
+                    leadingContent = Icon(item.icon)
+                  )
+                }
+              }
+            }
           }
 
           None -> {
           }
         }
       }
-      when(style) {
+      when (style) {
         is HeaderWithBackIcon -> {
           VerticalSpacer(space = 8)
           TUIDivider()
         }
+
         is HeaderWithTitle -> {
           VerticalSpacer(space = 16)
           TUIDivider()
         }
+
         is HeaderWithTrailingIcon -> {
           VerticalSpacer(space = 12)
           TUIDivider()
         }
+
         None -> {}
       }
     }
@@ -175,10 +229,10 @@ fun TUIMobileOverlayHeader(
 }
 
 data class TUIMobileOverlayHeaderTags(
-  val parentTag : String = "TUIMobileOverlayHeader_Parent",
-  val dividerTag : String = "TUIMobileOverlayHeader_Divider",
-  val leadingIconButtonTag : TUIIconButtonTags = TUIIconButtonTags(parentTag = "TUIMobileOverlayHeader_LeadingIcon"),
-  val trailingIconButtonTag : TUIIconButtonTags = TUIIconButtonTags(parentTag = "TUIMobileOverlayHeader_TrailingIcon"),
+  val parentTag: String = "TUIMobileOverlayHeader_Parent",
+  val dividerTag: String = "TUIMobileOverlayHeader_Divider",
+  val leadingIconButtonTag: TUIIconButtonTags = TUIIconButtonTags(parentTag = "TUIMobileOverlayHeader_LeadingIcon"),
+  val trailingIconButtonTag: TUIIconButtonTags = TUIIconButtonTags(parentTag = "TUIMobileOverlayHeader_TrailingIcon"),
 )
 
 @Composable
@@ -210,11 +264,11 @@ fun TUIMobileOverlayHeaderPreview() {
       )
       VerticalSpacer(space = 10)
       TUIMobileOverlayHeader(
-        modifier = Modifier.fillMaxWidth(), style = HeaderWithTrailingIcon(title = "Select Asset",
-          trailingIcon = TarkaIcons.Regular.Dismiss24,
-          onTrailingIconClick = {
-
-          })
+        modifier = Modifier.fillMaxWidth(),
+        style = HeaderWithTrailingIcon(title = "Select Asset",
+          trailingIcon = TarkaIcons.Regular.MoreHorizontal24,
+          onTrailingIconClick = {}
+        )
       )
       VerticalSpacer(space = 10)
       TUIMobileOverlayHeader(
