62513f7fc61688582cc7d6ab312a06b7c8e5a782
priyacs651 <priyacs651@gmail.com>
2024-06-13 17:36:41 +0530

    Removed AppBottomBar

--- tarka-ui/src/main/java/com/tarkalabs/tarkaui/components/TUIAppBottomBar.kt
+++ tarka-ui/src/main/java/com/tarkalabs/tarkaui/components/TUIAppBottomBar.kt
@@ -2,14 +2,21 @@ package com.tarkalabs.tarkaui.components
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tarkalabs.tarkaui.components.base.IconButtonSize
@@ -45,41 +52,53 @@ import com.tarkalabs.tarkaui.theme.TUITheme
 fun TUIAppBottomBar(
     modifier: Modifier = Modifier,
     icons: List<TarkaIcon> = emptyList(),
-    onClickEvent: (index: Int) -> Unit, tags: List<TUIIconButtonTags>,
-    color: Color = Color.Transparent,
+    onClickEvent: (index: Int) -> Unit,
+    tags: List<TUIIconButtonTags>,
 ) {
-    require(icons.size < 6 && tags.size < 6) { "icon and tags should not exceed more 6"}
+    require(icons.size <= 6 && tags.size <= 6) { "icon and tags should not exceed more 6" }
 
-    Column(
+    var selectedIconIndex by remember { mutableIntStateOf(1) } // Initially, the second icon is selected
+
+    Row(
         modifier = modifier
             .background(color = TUITheme.colors.surface)
             .fillMaxWidth()
-            .sizeIn(maxHeight = 64.dp)
-            .wrapContentHeight()
+            .height(64.dp)
+            .wrapContentHeight(),
+        horizontalArrangement = Arrangement.SpaceAround
     ) {
-        BottomAppBar(
-            containerColor = color,
-            modifier = Modifier.fillMaxWidth(),
-            actions = {
-                icons.forEachIndexed { index, icon ->
-                    TUIIconButton(
-                        modifier = Modifier.weight(1f),
-                        icon = icon,
-                        onIconClick = { onClickEvent(index) },
-                        tags = if (icons.size == tags.size) {
-                            tags[index]
-                        } else {
-                            TUIIconButtonTags()
-                        },
-                        iconButtonStyle = IconButtonStyle.Ghost,
-                        buttonSize = IconButtonSize.XL
-                    )
-                }
+        icons.forEachIndexed { index, icon ->
+            val isIconSelected = index == selectedIconIndex
+            Box(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                TUIIconButton(
+                    modifier = Modifier.wrapContentSize(),
+                    icon = icon,
+                    onIconClick = {
+                        selectedIconIndex = if (isIconSelected) -1 else index
+                        onClickEvent(selectedIconIndex)
+                    },
+                    tags = if (icons.size == tags.size) {
+                        tags[index]
+                    } else {
+                        TUIIconButtonTags()
+                    },
+                    iconButtonStyle = if (isIconSelected) {
+                        IconButtonStyle.Secondary
+                    } else {
+                        IconButtonStyle.Ghost
+                    },
+                    buttonSize = IconButtonSize.XL
+                )
             }
-        )
-        TUIDivider()
+        }
     }
+    TUIDivider()
 }
+
+
+
 
 @Preview
 @Composable
@@ -87,7 +106,61 @@ fun EamNormalBottomBar() {
     TUITheme {
         Column {
             TUIAppBottomBar(
-                icons =  listOf(
+                icons = listOf(
+                    TarkaIcons.Regular.SelectObjectSkew24,
+                    TarkaIcons.Regular.Lasso24,
+                    TarkaIcons.Regular.ArrowUndo24,
+                    TarkaIcons.Regular.ArrowRedo24,
+                    TarkaIcons.Regular.ArrowRedo24,
+                    TarkaIcons.Regular.ArrowRedo24,
+                ),
+                onClickEvent = { index ->
+                    when (index) {
+                        0 -> Log.d("test", "1 item clicked")
+                        1 -> Log.d("test", "2 item clicked")
+                        2 -> Log.d("test", "3 item clicked")
+                        3 -> Log.d("test", "4item clicked")
+                    }
+                },
+                tags = listOf(
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconOne"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconTwo"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconThree"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconFour"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconFour"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconFour"),
+
+                    )
+            )
+            VerticalSpacer(space = 16)
+
+            TUIAppBottomBar(
+                icons = listOf(
+                    TarkaIcons.Regular.SelectObjectSkew24,
+                    TarkaIcons.Regular.Lasso24,
+                    TarkaIcons.Regular.ArrowUndo24,
+                    TarkaIcons.Regular.ArrowRedo24,
+                    TarkaIcons.Regular.ArrowRedo24,
+                ),
+                onClickEvent = { index ->
+                    when (index) {
+                        0 -> Log.d("test", "1 item clicked")
+                        1 -> Log.d("test", "2 item clicked")
+                        2 -> Log.d("test", "3 item clicked")
+                        3 -> Log.d("test", "4item clicked")
+                    }
+                },
+                tags = listOf(
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconOne"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconTwo"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconThree"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconFour"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconFour"),
+                )
+            )
+            VerticalSpacer(space = 16)
+            TUIAppBottomBar(
+                icons = listOf(
                     TarkaIcons.Regular.SelectObjectSkew24,
                     TarkaIcons.Regular.Lasso24,
                     TarkaIcons.Regular.ArrowUndo24,
@@ -106,6 +179,63 @@ fun EamNormalBottomBar() {
                     TUIIconButtonTags(parentTag = "TUITopBar_itemIconTwo"),
                     TUIIconButtonTags(parentTag = "TUITopBar_itemIconThree"),
                     TUIIconButtonTags(parentTag = "TUITopBar_itemIconFour"),
+                )
+            )
+            VerticalSpacer(space = 16)
+            TUIAppBottomBar(
+                icons = listOf(
+                    TarkaIcons.Regular.SelectObjectSkew24,
+                    TarkaIcons.Regular.Lasso24,
+                    TarkaIcons.Regular.ArrowUndo24,
+                ),
+                onClickEvent = { index ->
+                    when (index) {
+                        0 -> Log.d("test", "1 item clicked")
+                        1 -> Log.d("test", "2 item clicked")
+                        2 -> Log.d("test", "3 item clicked")
+                        3 -> Log.d("test", "4item clicked")
+                    }
+                },
+                tags = listOf(
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconOne"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconTwo"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconThree"),
+                )
+            )
+            VerticalSpacer(space = 16)
+            TUIAppBottomBar(
+                icons = listOf(
+                    TarkaIcons.Regular.SelectObjectSkew24,
+                    TarkaIcons.Regular.Lasso24,
+                ),
+                onClickEvent = { index ->
+                    when (index) {
+                        0 -> Log.d("test", "1 item clicked")
+                        1 -> Log.d("test", "2 item clicked")
+                        2 -> Log.d("test", "3 item clicked")
+                        3 -> Log.d("test", "4item clicked")
+                    }
+                },
+                tags = listOf(
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconOne"),
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconTwo"),
+                )
+            )
+            VerticalSpacer(space = 16)
+            TUIAppBottomBar(
+                icons = listOf(
+                    TarkaIcons.Regular.SelectObjectSkew24,
+                ),
+                onClickEvent = { index ->
+                    when (index) {
+                        0 -> Log.d("test", "1 item clicked")
+                        1 -> Log.d("test", "2 item clicked")
+                        2 -> Log.d("test", "3 item clicked")
+                        3 -> Log.d("test", "4item clicked")
+                    }
+                },
+                tags = listOf(
+                    TUIIconButtonTags(parentTag = "TUITopBar_itemIconOne"),
                 )
             )
         }
